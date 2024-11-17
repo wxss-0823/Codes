c78aca5824b29792da6385e046421ceb6be86879
wxss <2198993328@qq.com>
2024-11-17 23:38:04 +0800

    wxss: 24-11-17 23:38:03

--- VSCode/PHP/test.php
+++ VSCode/PHP/test.php
@@ -4,7 +4,7 @@
 
 <?php
 echo "Hello World!";
-?>
+?><br>
 
 <?php
 $conn = new mysqli("localhost", "root", "020823");
@@ -12,6 +12,32 @@ if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 }
 echo "连接成功";
-?>
+?><br>
 
+<?php
+    $x = 1;
+    $y = 2;
+    function test() {
+        // 使用 global 关键字使用全局变量
+        global $x,$y;
+        echo $x + $y;
+
+        // 使用 GLOBALS 数组访问全局变量
+        echo $GLOBALS['y'] + $GLOBALS['x'];
+    }
+
+    test();
+?><br>
+
+<?php
+    function add() {
+        static $z = 1;
+        echo $z ;
+        $z++ ;
+    }
+    add();
+    add();
+    add();
+    add();
+?>
 </body>

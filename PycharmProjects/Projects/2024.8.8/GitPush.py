06f071070846d2f7e39afc180ac77512ea29b3cf
wxss <2198993328@qq.com>
2024-12-08 15:15:50 +0800

    wxss: Update pycharm gitpush. On Sun Dec 08 15:15:48 2024

--- PycharmProjects/Projects/2024.8.8/GitPush.py
+++ PycharmProjects/Projects/2024.8.8/GitPush.py
@@ -15,7 +15,7 @@ def GetTime() -> str:
 author = "wxss"
 update_msg = input("Please input the update message: ")
 if update_msg == "exit" or "q" or "quit":
-    os.system("exit")
+    os.system("exit\n")
 commit_time = GetTime()
 commit_msg = f"\"{author}: {update_msg} On {commit_time}\""
 
@@ -35,32 +35,39 @@ os.system(check_cmd)
 print("[CMD_2] " + storage_cmd)
 os.system(storage_cmd)
 
-def switch(case) -> int:
-    if case == "q" or "exit" or "quit":
+def switch(case) -> int :
+    if case == "q" or "exit" or "quit" :
         print("Wait for 5s to exit...")
         time.sleep(5)
         return 0
-    elif case == "r":
+    elif case == "r" :
         return 2
-    else:
+    else :
         switch(input("Please input correct command: "))
 
-while True:
-    try:
+def input_exit() -> None :
+    if input("Please input q/quit/exit to exit: ") == "q" or "quit" or "exit" :
+        os.system("exit\n")
+    else :
+        input_exit()
+
+while True :
+    try :
         # 提交信息及文件
         print("[CMD_3] " + commit_cmd)
         os.system(commit_cmd)
         # push 到远程库
         print("[CMD_4] " + push_cmd)
-        if not os.system(push_cmd):
-            raise Exception("Git failed!")
-        else:
+        if not os.system(push_cmd) :
             # 结束信息及退出
             print("[End] finish uploading files to remote repository!\n")
+            input_exit()
             break
-    except Exception as e:
-        if switch(input("Please input q/quit/exit to exit, or input r to retry: ")) == 0:
+        else :
+            raise Exception("Git failed!")
+    except Exception as e :
+        if switch(input("Please input q/quit/exit to exit, or input r to retry: ")) == 0 :
             print(f"[End] {e}")
             break
-        else:
+        else :
             pass

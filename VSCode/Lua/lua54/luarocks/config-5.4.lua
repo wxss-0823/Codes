456e7eab5d4a9ded66835f28531ea22c98382213
wxss <2198993328@qq.com>
2024-12-17 01:34:42 +0800

    wxss: Update Lua structure On Tue Dec 17 01:34:39 2024

---
+++ VSCode/Lua/lua54/luarocks/config-5.4.lua
@@ -0,0 +1,7 @@
+lua_interpreter = "lua54.exe"
+variables = {
+   LUA_BINDIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua",
+   LUA_DIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua",
+   LUA_INCDIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua/include",
+   LUA_LIBDIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua"
+}

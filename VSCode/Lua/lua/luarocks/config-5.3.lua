36839207ab804a1ee37ced5b22e4db951435165c
wxss <2198993328@qq.com>
2024-12-17 23:55:31 +0800

    wxss: Update lua codes On Tue Dec 17 23:55:29 2024

--- VSCode/Lua/lua/luarocks/config-5.3.lua
+++
@@ -1,7 +0,0 @@
-lua_interpreter = "lua53.exe"
-variables = {
-   LUA_BINDIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua",
-   LUA_DIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua",
-   LUA_INCDIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua/include",
-   LUA_LIBDIR = "D:/Users/ProjectFiles/Codes/VSCode/Lua/lua"
-}
--- VSCode/Lua/lua54/code/metatableTest.lua
+++
@@ -1,9 +0,0 @@
-mymetatable = {}
-mytable = setmetatable({key = "value"}, {__newindex = 
-    function (t, k, v)
-        rawset(mymetatable, k, v .. t.key)
-    end
-})
-
-mytable.xxx = "yyy"
-print(mymetatable.xxx, mytable.xxx)

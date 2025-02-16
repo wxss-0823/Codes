926fb97c2460c0c44d5de3045722667877872f21
wxss <2198993328@qq.com>
2025-02-16 23:21:14 +0800

    wxss: Update Vue Project skeleton On Sun Feb 16 23:20:43 2025

---
+++ WebStorm/VueGuide/BasicLearning/VueProject/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloVue from "./components/HelloVue.vue"
+</script>
+
+<template>
+  <header>
+    <div>
+
+    </div>
+  </header>
+
+  <main>
+    <HelloVue msg="Hello Vue!" />
+  </main>
+</template>
+
+<style scoped>
+
+</style>

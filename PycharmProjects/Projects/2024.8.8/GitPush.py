903a98ab2cfc7d4ca2fe5d72a64e45e161b63418
wxss <2198993328@qq.com>
2025-08-13 18:22:26 +0800

    wxss: Update python gitpush On Wed Aug 13 18:22:26 2025

--- PycharmProjects/Projects/2024.8.8/GitPush.py
+++ PycharmProjects/Projects/2024.8.8/GitPush.py
@@ -7,25 +7,25 @@ import os
 import time
 
 
-def GetTime() -> str:
-    return time.strftime("%a %b %d %H:%M:%S %Y", time.localtime())
+def get_time() -> str:
+  return time.strftime("%a %b %d %H:%M:%S %Y", time.localtime())
 
 def switch(case) -> int:
-    if case in exit_dict:
-        print("Wait for 5s to exit...")
-        time.sleep(5)
-        return 0
-    elif case == "r" :
-        return 2
-    else :
-        switch(input("Please input correct command: "))
-        return -1
+  if case in exit_dict:
+    print("Wait for 5s to exit...")
+    time.sleep(5)
+    return 0
+  elif case == "r" :
+    return 2
+  else :
+    switch(input("Please input correct command: "))
+    return -1
 
 def input_exit(cmd) -> None :
-    if cmd in exit_dict:
-        os.system("exit")
-    else :
-        input_exit(input("Please input correct command: "))
+  if cmd in exit_dict:
+    os.system("exit")
+  else :
+    input_exit(input("Please input correct command: "))
 
 # 终端信息
 check_cmd = "git status"
@@ -37,7 +37,7 @@ os.system(check_cmd)
 # 提交信息
 author = "wxss"
 update_msg = input("Please input the update message: ")
-commit_time = GetTime()
+commit_time = get_time()
 commit_msg = f"\"{author}: {update_msg} On {commit_time}\""
 
 # 命令集
@@ -47,29 +47,29 @@ push_cmd = "git push -u origin master"
 exit_dict = ["exit", "q", "\n", "quit"]
 
 if update_msg in exit_dict:
-    os.system("exit")
+  os.system("exit")
 else:
-    # 存储文件
-    print("[CMD_2] " + storage_cmd)
-    os.system(storage_cmd)
+  # 存储文件
+  print("[CMD_2] " + storage_cmd)
+  os.system(storage_cmd)
 
-    while True :
-        try :
-            # 提交信息及文件
-            print("[CMD_3] " + commit_cmd)
-            os.system(commit_cmd)
-            # push 到远程库
-            print("[CMD_4] " + push_cmd)
-            if not os.system(push_cmd) :
-                # 结束信息及退出
-                print("[End] finish uploading files to remote repository!")
-                input_exit(input("Please input q/quit/exit to exit: "))
-                break
-            else :
-                raise Exception("Git failed!")
-        except Exception as e :
-            if switch(input("Please input q/quit/exit to exit, or input r to retry: ")) == 0 :
-                print(f"[End] {e}")
-                break
-            else :
-                pass
+  while True :
+    try :
+      # 提交信息及文件
+      print("[CMD_3] " + commit_cmd)
+      os.system(commit_cmd)
+      # push 到远程库
+      print("[CMD_4] " + push_cmd)
+      if not os.system(push_cmd) :
+        # 结束信息及退出
+        print("[End] finish uploading files to remote repository!")
+        input_exit(input("Please input q/quit/exit to exit: "))
+        break
+      else :
+        raise Exception("Git failed!")
+    except Exception as e :
+      if switch(input("Please input q/quit/exit to exit, or input r to retry: ")) == 0 :
+        print(f"[End] {e}")
+        break
+      else :
+        pass

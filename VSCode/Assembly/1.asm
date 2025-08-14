cd39e9644fa33e28ba743abf0108fc1f1243e06a
Wxss <2198993328@qq.com>
2025-08-14 19:21:27 +0800

    wxss: Add assembly codes On Thu Aug 14 19:21:27 2025

--- VSCode/Assembly/1.asm
+++ VSCode/Assembly/1.asm
@@ -1,11 +1,11 @@
 assume cs:codescg
 
 codescg segment
-  mov ax, 0123h
-  mov bx, 0456h
-  add ax, ax
-  mov ax, 4c00h
-  int 21h
+          mov ax, 0123h
+          mov bx, 0456h
+          add ax, ax
+          mov ax, 4c00h
+          int 21h
 
 codescg ends
 

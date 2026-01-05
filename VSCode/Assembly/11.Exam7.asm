0f0b36a63979df5afd2d71dc180f43086e30b888
wxss <2198993328@qq.com>
2026-01-06 01:23:25 +0800

    wxss: Update assembly code On Tue Jan 06 01:23:25 2026

---
+++ VSCode/Assembly/11.Exam7.asm
@@ -0,0 +1,68 @@
+assume cs:code
+data segment
+       db '1975','1976','1977','1978','1979','1980','1981','1982','1983'
+       db '1984','1985','1986','1987','1988','1989','1990','1991','1992'
+       db '1993','1994','1995'
+  ;以上是表示 21 年的 21 个字符串
+       dd 16,22,382,1356,2390,8000,16000,24486,50065,97479,140417,197514
+       dd 345980,590827,803530,1183000,1843000,2759000,3753000,4649000,5937000
+  ;以上是表示 21 年公司总收入的 21 个 dword 型数据
+       dw 3,7,9,13,28,38,130,220,476,778,1001,1442,2258,2793,4037,5635,8226
+       dw 11542,14430,15257,17800
+  ;以上是表示 21 年公司雇员人数的 21 个 word 型数据
+data ends
+
+table segment
+        db 21 dup ('xxxx xxxx xx xx ')
+table ends
+
+code segment
+  start:mov  ax, data        ; 段地址定位
+        mov  ds, ax
+        mov  ax, table
+        mov  es, ax
+
+        mov  si, 0
+        mov  di, 84
+        mov  bp, 168
+        mov  bx, 0
+        mov  cx, 21
+        
+  s:    push cx              ; 计算当前行数对应的内存 offset
+        mov  ax, 21
+        sub  ax, cx
+        mov  cx, ax
+        mov  ax, 16
+        mul  cl
+        mov  bx, ax          ; 计算结果入栈
+
+        mov  ax, ds:[si]
+        mov  es:[bx], ax
+        add  si, 2
+        mov  ax, ds:[si]
+        mov  es:[bx+2], ax
+        add  si, 2
+
+        mov  ax, ds:[di]
+        push ax              ; L16
+        mov  es:[bx+5], ax
+        add  di, 2
+        mov  ax, ds:[di]
+        push ax              ; H16
+        mov  es:[bx+7], ax
+        add  di, 2
+
+        mov  ax, ds:[bp]
+        mov  es:[bx+10], ax
+        add  bp, 2
+
+        mov  cx, ax
+        pop  dx
+        pop  ax
+        div  cx
+        mov  es:[bx+13], ax
+
+        pop  cx
+        loop s
+code ends
+end start

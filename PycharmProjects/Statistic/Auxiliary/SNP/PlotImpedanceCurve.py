e5a4bc96b17fe6a00f0de2005726dc13a108a1cc
Apology <2198993328@qq.com>
2026-09-24 17:37:43 +0800

    Add files via upload

---
+++ PycharmProjects/Statistic/Auxiliary/SNP/PlotImpedanceCurve.py
@@ -0,0 +1,14 @@
+#!D:\Users\Anaconda\anaconda3\python.exe
+# -*- coding: utf-8 -*-
+# @Time    : 2026/2/10 20:20
+# @Author  : Coffee
+# @Project : Auxiliary
+# @File    : PlotImpedanceCurve.py
+
+import pandas as pd
+
+csv_dir = r"C:\Users\w00025121\Desktop\共模2026-02-10_17-31-37.csv"
+curve_df = pd.read_csv(csv_dir)
+
+
+
